02bf7ef7ca6201349639cab43eff00a3a3b29e90
ducanhn04859 <ducanhn1308@gmail.com>
2021-09-02 21:21:16 +0700

    modified port and opend graphQL port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,47 @@
-require('dotenv').config()
-import 'reflect-metadata'
-import express from 'express'
-import { createConnection } from 'typeorm'
-import { User } from './entities/user'
-import { Post } from './entities/post'
+/** @format */
+
+require("dotenv").config()
+import "reflect-metadata"
+import express from "express"
+import { createConnection } from "typeorm"
+import { User } from "./entities/User"
+import { Post } from "./entities/Post"
+import { ApolloServer } from "apollo-server-express"
+import { buildSchema } from "type-graphql"
+import { HelloResolver } from "./resolvers/hello"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 const main = async () => {
-    await createConnection({
-      type: "postgres",
-      database: "reddit",
-      username: process.env.DB_USERNAME_DEV,
-      password: process.env.DB_PASSWORD_DEV,
-      logging: true,
-      synchronize: true,
-      entities:[User, Post]
-    })
+  await createConnection({
+    type: "postgres",
+    database: "reddit",
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    logging: true,
+    synchronize: true,
+    entities: [User, Post],
+  })
 
-    
-    const app = express()
-    
-    app.listen(4000, () => console.log('Server is running in port 4000'))
-    
+  const app = express()
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    // context: ({ req, res }): Context => ({ req, res }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  })
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app, cors: false })
+
+  const PORT = process.env.PORT || 4000
+
+  app.listen(PORT, () =>
+    console.log(
+      `Server is running in port ${PORT}. GraphQL server started on localhost:${PORT}${apolloServer.graphqlPath}`
+    )
+  )
 }
-main().catch(err =>console.log(err))
+main().catch((err) => console.log(err))
